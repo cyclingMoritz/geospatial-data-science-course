5ef57c273149d7e8e97e913955dcc70fedfae5b2
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-11-25 23:20:49 +0100

    Load municipal boundary

---
+++ capstone/modules/boundary.py
@@ -0,0 +1,13 @@
+import osmnx as ox
+
+def load_boundary(city="Lisbon, Portugal", epsg=4326):
+    gdf = ox.geocode_to_gdf(city)
+    gdf = gdf.to_crs(epsg=epsg)
+
+    union = gdf.unary_union
+    if union.geom_type == "MultiPolygon":
+        boundary = max(union, key=lambda p: p.area)
+    else:
+        boundary = union
+
+    return gdf, boundary
