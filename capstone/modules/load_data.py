0a82ec40c38dd0bb7b30343b30f691ecbf7d6976
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-11-26 13:21:47 +0100

    Combine load data and get boundary

--- capstone/modules/load_data.py
+++ capstone/modules/load_data.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import osmnx as ox
 
 def load_accident_data(path="data/Road_Accidents_Lisbon.csv"):
     df = pd.read_csv(path)
     return df
+
+def load_boundary(city="Lisbon, Portugal", epsg=4326):
+    gdf = ox.geocode_to_gdf(city)
+    gdf = gdf.to_crs(epsg=epsg)
+
+    union = gdf.unary_union
+    if union.geom_type == "MultiPolygon":
+        boundary = max(union, key=lambda p: p.area)
+    else:
+        boundary = union
+
+    return gdf, boundary
