94908ba88865ae5a3b6b39fe40bad6efbe0ceeaa
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-11-26 13:27:10 +0100

    Move date creation to clea_data function

--- capstone/modules/stats.py
+++ capstone/modules/stats.py
@@ -1,3 +1,17 @@
+import pandas as pd
+
+def load_data(df):
+    clean_data = df.copy()
+    
+    #Generate the date attribute
+    date_str = clean_data["day"].astype(str).str.zfill(2) + " " + clean_data["month"].astype(str) + " 2023"
+    clean_data["date"] = pd.to_datetime(date_str, errors="coerce")
+    clean_data["date_str"] = clean_data["date"].dt.strftime("%d %b %Y")
+
+    # Make temporal attributes ordered
+    return clean_data
+
+
 def compute_overview(df):
     return {
         "Number accidents": len(df),
